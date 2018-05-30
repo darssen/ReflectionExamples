f978d88b914ad2bfc97ed39c3802426497ae4abc
juan <darssen@gmail.com>
2018-05-31 00:30:02 +0200

    Pushing changes including Oracle copyright for ClassSpy

--- Reflection/src/com/darssen/reflection/ClassSpy.java
+++ Reflection/src/com/darssen/reflection/ClassSpy.java
@@ -1,106 +1,115 @@
 package com.darssen.reflection;
+/*
+ * Copyright (c) 1995, 2008, Oracle and/or its affiliates. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *   - Neither the name of Oracle or the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ *Given a class name and an indication of which members are of interest, the ClassSpy
+ *example uses the get*s() methods to determine the list of all public elements, including any which are inherited.
+ *
+ * Samples of how to call (Basically needs the class to inspect as argument)
+ * 
+ * 		java ClassSpy java.lang.ClassCastException CONSTRUCTOR
+ * 		java ClassSpy java.nio.channels.ReadableByteChannel METHOD
+ */ 
+
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
 import java.lang.reflect.Member;
 import static java.lang.System.out;
 
-import java.lang.annotation.Annotation;
+enum ClassMember { CONSTRUCTOR, FIELD, METHOD, CLASS, ALL }
 
-
-enum ClassMember { CONSTRUCTOR, FIELD, METHOD, CLASS, ANNOTATIONS, ALL }
-
-@Deprecated
 public class ClassSpy {
-	public static void main(String... args) {
-		try {
-			Class<?> c = Class.forName(args[0]);
-			out.format("Class:%n  %s%n%n", c.getCanonicalName());
+    public static void main(String... args) {
+	try {
+	    Class<?> c = Class.forName(args[0]);
+	    out.format("Class:%n  %s%n%n", c.getCanonicalName());
 
-			Package p = c.getPackage();
-			out.format("Package:%n  %s%n%n",
-					(p != null ? p.getName() : "-- No Package --"));
+	    Package p = c.getPackage();
+	    out.format("Package:%n  %s%n%n",
+		       (p != null ? p.getName() : "-- No Package --"));
 
-			for (int i = 1; i < args.length; i++) {
-				switch (ClassMember.valueOf(args[i])) {
-				case CONSTRUCTOR:
-					printMembers(c.getConstructors(), "Constructor");
-					break;
-				case FIELD:
-					printMembers(c.getDeclaredFields(), "Fields");
-					break;
-				case ANNOTATIONS:
-					printAnnotations(c.getDeclaredAnnotations(), "Annotations");
-					break;
-				case METHOD:
-					printMembers(c.getMethods(), "Methods");
-					break;
-				case CLASS:
-					printClasses(c);
-					break;
-				case ALL:
-					printMembers(c.getConstructors(), "Constuctors");
-					printMembers(c.getDeclaredFields(), "Fields");
-					printMembers(c.getMethods(), "Methods");
-					printClasses(c);
-					break;
-				default:
-					assert false;
-				}
-			}
-
-			// production code should handle these exceptions more gracefully
-		} catch (ClassNotFoundException x) {
-			x.printStackTrace();
+	    for (int i = 1; i < args.length; i++) {
+		switch (ClassMember.valueOf(args[i])) {
+		case CONSTRUCTOR:
+		    printMembers(c.getConstructors(), "Constructor");
+		    break;
+		case FIELD:
+		    printMembers(c.getFields(), "Fields");
+		    break;
+		case METHOD:
+		    printMembers(c.getMethods(), "Methods");
+		    break;
+		case CLASS:
+		    printClasses(c);
+		    break;
+		case ALL:
+		    printMembers(c.getConstructors(), "Constuctors");
+		    printMembers(c.getFields(), "Fields");
+		    printMembers(c.getMethods(), "Methods");
+		    printClasses(c);
+		    break;
+		default:
+		    assert false;
 		}
-	}
-	private static void printMembers(Member[] mbrs, String s) {
-		out.format("%s:%n", s);
-		for (Member mbr : mbrs) {
-			if (mbr instanceof Field){
-				Field f = (Field)mbr;
-				out.format("  %s%n", f.toGenericString());
-				System.out.format("Type: %s%n", f.getType());
-				out.format("Fields in Class '%s' containing modifiers:  %s%n",
-					       f.getClass().getName(),
-					       Modifier.toString(f.getModifiers()));
-				out.format("%-8s [ synthetic=%-5b enum_constant=%-5b ]%n",
-					       f.getName(), f.isSynthetic(),
-					       f.isEnumConstant());
-			    System.out.format("GenericType: %s%n", f.getGenericType());
-				out.format("  -- declared in: %s%n", f.getDeclaringClass());
-			}
-			else if (mbr instanceof Constructor){
-				Constructor c = (Constructor)mbr;
-				out.format("  %s%n", c.toGenericString());
-				out.format("  -- declared in: %s%n", c.getDeclaringClass());
-			}else if (mbr instanceof Method){
-				Method m = ((Method)mbr);
-				out.format("  %s%n", m.toGenericString());
-				out.format("  -- declared in: %s%n", m.getDeclaringClass());
-			}
-		}
-		if (mbrs.length == 0)
-			out.format("  -- No %s --%n", s);
-		out.format("%n");
-	}
+	    }
 
-	private static void printAnnotations(Annotation[] annotations, String s){
-		out.format("%s:%n", s);
-		for(Annotation annotation:annotations){
-			out.format("  %s%n", annotation.toString());
-		}
-		
-		
+        // production code should handle these exceptions more gracefully
+	} catch (ClassNotFoundException x) {
+	    x.printStackTrace();
 	}
-	private static void printClasses(Class<?> c) {
-		out.format("Classes:%n");
-		Class<?>[] clss = c.getClasses();
-		for (Class<?> cls : clss)
-			out.format("  %s%n", cls.getCanonicalName());
-		if (clss.length == 0)
-			out.format("  -- No member interfaces, classes, or enums --%n");
-		out.format("%n");
+    }
+
+    private static void printMembers(Member[] mbrs, String s) {
+	out.format("%s:%n", s);
+	for (Member mbr : mbrs) {
+	    if (mbr instanceof Field)
+		out.format("  %s%n", ((Field)mbr).toGenericString());
+	    else if (mbr instanceof Constructor)
+		out.format("  %s%n", ((Constructor)mbr).toGenericString());
+	    else if (mbr instanceof Method)
+		out.format("  %s%n", ((Method)mbr).toGenericString());
 	}
+	if (mbrs.length == 0)
+	    out.format("  -- No %s --%n", s);
+	out.format("%n");
+    }
+
+    private static void printClasses(Class<?> c) {
+	out.format("Classes:%n");
+	Class<?>[] clss = c.getClasses();
+	for (Class<?> cls : clss)
+	    out.format("  %s%n", cls.getCanonicalName());
+	if (clss.length == 0)
+	    out.format("  -- No member interfaces, classes, or enums --%n");
+	out.format("%n");
+    }
 }
